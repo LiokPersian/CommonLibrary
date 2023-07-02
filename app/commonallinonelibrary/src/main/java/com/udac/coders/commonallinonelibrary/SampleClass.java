947df123653c5c7c0e5bd3cea76169ab57de2c39
LiokPersian <44110492+LiokPersian@users.noreply.github.com>
2023-07-02 15:31:30 +0530

    Initial commit

---
+++ app/commonallinonelibrary/src/main/java/com/udac/coders/commonallinonelibrary/SampleClass.java
@@ -0,0 +1,17 @@
+package com.udac.coders.commonallinonelibrary;
+
+public class SampleClass {
+    public SampleClass(){
+
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String Name;
+}
